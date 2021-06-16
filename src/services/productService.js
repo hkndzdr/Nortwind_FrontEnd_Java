7181d94efe3037eeaa839992d4dcc94d31dc4534
Hakan Dizdar <hakandizdar@gmail.com>
2021-06-17 00:24:32 +0300

    16.06.2021

--- src/services/productService.js
+++ src/services/productService.js
@@ -4,4 +4,13 @@ export default class ProductService {
   getProducts() {
     return axios.get("http://localhost:8080/api/products/getall");
   }
+
+  getByProductId(productId) {
+    return axios.get("http://localhost:8080/api/products/getById?id="+ productId);
+  }
+
+  getByProductName(productName) {
+    return axios.get("http://localhost:8080/api/products/getByProductName?productName="+ productName);
+  }
+
 }
