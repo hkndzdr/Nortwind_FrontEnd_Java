751d645bddab64a8bd6d5d31d40455c73f576a1b
Hakan Dizdar <hakandizdar@gmail.com>
2021-06-23 23:13:44 +0300

    23.06

---
+++ src/pages/ProductAdd.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { Button, FormField, Label } from "semantic-ui-react";
+import KodlamaIoTextInput from "../utilities/customFormControls/KodlamaIoTextInput";
+
+export default function ProductAdd() {
+  const initialValues = { productName: "", unitPrice: 10 };
+  const schema = Yup.object({
+    productName: Yup.string().required("Ürün adı zorunlu"),
+    unitPrice: Yup.number().required("Ürün fiyatı zorunlu"),
+  });
+  return (
+    <div>
+      <Formik initialValues={initialValues} 
+      validationSchema={schema}
+      onSubmit = {(values)=>{
+          console.log(values)
+      }}
+      >
+        <Form className="ui form">
+            <KodlamaIoTextInput name="productName" placeholder="Ürün adı"></KodlamaIoTextInput>
+            <KodlamaIoTextInput name="unitPrice" placeholder="Ürün fiyatı"></KodlamaIoTextInput>
+          <Button color="green" type="submit">Ekle</Button>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
